8f53e38a4a16b3d9bad44dd033fdeeb147b54884
fdbraconi1 <federicobraconi3@gmail.com>
2015-04-08 00:12:07 +0200

    Create Rubrica.java

---
+++ Rubrica.java
@@ -0,0 +1,34 @@
+package rubrica;
+
+import java.util.Vector;
+import java.util.Vector;
+
+public class Rubrica {
+	private Vector rubrica;
+		
+	public Rubrica(){
+		rubrica=new Vector(1,1);
+	}
+	
+	public void aggiungiVoce(Voce v){
+		rubrica.addElement(v);
+	}
+	
+	public void eliminaVoce(int posizione){
+		try{
+			rubrica.removeElementAt(posizione);
+			System.out.println("Eliminazione avvenuta con successo");
+		}
+		catch(Exception e){
+			System.out.println("Errore nell'eliminazione");
+		}
+	}
+	
+	public void visualizzaRubrica(){
+		Voce v;
+		for(int i=0;i<rubrica.size();i++){
+			v = (Voce) rubrica.elementAt(i);
+			System.out.println("Posizione:  "+i+"  "+v.toString());
+		}
+	}
+}
